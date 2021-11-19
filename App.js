37bbc8f2503d54380d9845e7ac0f4a3653b24dec
rachidscorpion <38715551+rachidscorpion@users.noreply.github.com>
2021-11-19 16:16:28 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import {createStackNavigator} from 'react-navigation-stack'
+import {createAppContainer} from 'react-navigation'
+import HomeScreen from './src/screens/HomeScreen'
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: "My App"
+    }
+  }
+)
+
+
+export default createAppContainer(navigator)
