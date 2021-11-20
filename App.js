6af1e76c756d3550d75085e85ac25d8429ebec75
rachidscorpion <38715551+rachidscorpion@users.noreply.github.com>
2021-11-20 13:16:29 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+// Multiple Screen with react navigation
+// To install dependency of react naviagation Run command npm install react-navigation or npm install react-native-screens if first dosent work
+
+
 import {createStackNavigator} from 'react-navigation-stack'
 import {createAppContainer} from 'react-navigation'
 import HomeScreen from './src/screens/HomeScreen'
