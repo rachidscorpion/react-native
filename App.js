dd6038492ef261869f000370af1c6a222deadd00
rachidscorpion <38715551+rachidscorpion@users.noreply.github.com>
2021-11-20 16:59:03 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
-// Multiple Screen with react navigation
-// To install dependency of react naviagation Run command npm install react-navigation or npm install react-native-screens if first dosent work
+// Multiple Screen with react navigation and application structure like header footer or ham-menu
+
+    // 1. Install React Navigation
+    // npm install react-navigation
+    // or
+    // yarn add react-navigation
+
+    // 2. Install Dependencies
+    // expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+
+    // 3. Install React Navigation Stack
+    // npm install react-navigation-stack @react-native-community/masked-view
+    // or
+    // yarn add react-navigation-stack @react-native-community/masked-view
+
+    // 4. Start the app and clear cache with expo r -c
 
 
 import {createStackNavigator} from 'react-navigation-stack'
